1a0a4b0145baf0cef64ddfa1ac999751dca1a379
Aum Bhatt <aum@deriv.com>
2024-07-02 17:16:23 +0400

    refactor: PoaScreen to Poa module - renamed PoaScreen to Poa module - refactored AddressSection with Formik and moved it to accounts/modules

---
+++ packages/wallets/src/features/accounts/modules/Poa/components/AddressSection/AddressSection.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useFormikContext } from 'formik';
+import { FormField, InlineMessage, WalletDropdown, WalletText } from '../../../../../../components';
+import { TAddressDetails } from '../../types';
+import './AddressSection.scss';
+
+const AddressSection: React.FC = () => {
+    const { setFieldValue, status, values } = useFormikContext<TAddressDetails>();
+
+    return (
+        <div className='wallets-address-section'>
+            <div className='wallets-address-section__title'>
+                <WalletText weight='bold'>Address</WalletText>
+                <div className='wallets-address-section__title__divider' />
+            </div>
+            <div className='wallets-address-section__inline'>
+                <InlineMessage size='md' type='warning' variant='contained'>
+                    <div className='wallets-address-section__inline-message'>
+                        For faster verification, input the same address here as in your proof of address document (see
+                        section below)
+                    </div>
+                </InlineMessage>
+            </div>
+            <div className='wallets-address-section__input'>
+                <FormField
+                    label='First line of address*'
+                    name='firstLine'
+                    // validationSchema={addressFirstLineValidator}
+                />
+                <FormField
+                    label='Second line of address (optional)'
+                    name='secondLine'
+                    // validationSchema={addressSecondLineValidator}
+                />
+                <FormField label='Town/City*' name='townCityLine' /* validationSchema={cityValidator} */ />
+                <WalletDropdown
+                    label='State/Province'
+                    list={status.statesList}
+                    listHeight='sm'
+                    name='stateProvinceLine'
+                    onSelect={selectedItem => setFieldValue('stateProvinceDropdownLine', selectedItem)}
+                    value={values.stateProvinceLine ?? ''}
+                />
+                <FormField label='Postal/ZIP code' name='zipCodeLine' /* validationSchema={postcodeValidator} */ />
+            </div>
+        </div>
+    );
+};
+
+export default AddressSection;
